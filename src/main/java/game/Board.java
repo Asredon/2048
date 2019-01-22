8551fdc59ea46ef34b7aefd7428918c141a615c2
jscharein <jscharein@voipfuture.com>
2019-01-22 13:32:20 +0100

    seems finished for now

--- src/main/java/game/Board.java
+++ src/main/java/game/Board.java
@@ -1,31 +1,35 @@
 package game;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Board {
 
-    private int height;
-    private int width;
+    private int y;
+    private int x;
     private boolean win;
-    private Tile[][] tiles;
+    private boolean lose;
+    private int score;
+    private int[][] tiles;
 
-    public Board(int width, int height){
-        if (width < 4 || height < 4) {
+    public Board(int x, int y){
+        if (x < 4 || y < 4) {
             throw new AssertionError("The board cannot be smaller than 4x4");
         }
-
-        this.tiles = new Tile[width][height];
+        this.x = x;
+        this.y = y;
+        this.score = 0;
+        this.tiles = new int[this.x][this.y];
     }
 
-    public List<Tile> checkSpace(){
-        List<Tile> freeSpace = new ArrayList<Tile>(16);
-        for(int x = 0; x < this.width; x++){
-            for (Tile[] tile: tiles) {
-                for (Tile t: tile ){
-                    if (t.getValue() == 0) {
-                        freeSpace.add(t);
-                    }
+    public List<Integer[]> checkSpace(){
+        List<Integer[]> freeSpace = new ArrayList<Integer[]>(this.x *this.y);
+        for(int x = 0; x < this.x; x++) {
+            for (int y = 0; y < this.y; y++) {
+                if (tiles[x][y] == 0) {
+                    Integer[] coordinates = {x,y};
+                    freeSpace.add(coordinates);
                 }
             }
         }
@@ -36,26 +40,15 @@ public class Board {
         return checkSpace().size() == 0;
     }
 
-    public boolean isWin() {
-        for(int x = 0; x < this.width; x++) {
-            for (int y = 0; y < this.height; y++) {
-                if (tiles[x][y].getValue() == 2048) {
-                    return win = true;
-                }
-            }
-        }
-            return win = false;
-    }
-
-    public boolean canMove(){
+    public boolean movePossible(){
         if (!isBoardFull()){
             return true;
         }
-        for(int x = 0; x < this.width; x++) {
-            for (int y = 0; y < this.height; y++) {
-                Tile tile = tiles[x][y];
-                if(x < 3 && tile.getValue() == tiles[x+1][y].getValue() ||
-                        y < 3 && tile.getValue() == tiles[x][y+1].getValue()){
+        for(int x = 0; x < this.x; x++) {
+            for (int y = 0; y < this.y; y++) {
+                int tile = tiles[x][y];
+                if(x < 3 && tile == tiles[x+1][y] ||
+                        y < 3 && tile == tiles[x][y+1]){
                     return true;
                 }
             }
@@ -63,29 +56,170 @@ public class Board {
         return false;
     }
 
-    public void addTile(){
-        List<Tile> list = checkSpace();
-        if(!checkSpace().isEmpty()){
-            int i = (int) ((Math.random() * list.size()) % list.size());
-            Tile newTile = list.get(i);
-            newTile.setValue(Math.random() < 0.9 ? 2 : 4);
+    public void left(){
+        boolean needAddTile = false;
+        for (int x = 0; x < this.x; x++){
+            Integer[] line = getLine(x);
+            Integer[] merged = mergeLine(moveLine(line));
+            setLine(x, merged);
+            if(!needAddTile && !compareLine(line, merged)){
+                needAddTile = true;
+            }
+        }
+        if(needAddTile){
+            addTile();
         }
     }
 
-    public void moveLine(){
+    public void up(){
+        tiles = rotate();
+        tiles = rotate();
+        tiles = rotate();
+        left();
+        tiles = rotate();
+    }
+
+    public void right(){
+        tiles = rotate();
+        tiles = rotate();
+        left();
+        tiles = rotate();
+        tiles = rotate();
+    }
+
+    public void down(){
+        tiles = rotate();
+        left();
+        tiles = rotate();
+        tiles = rotate();
+        tiles = rotate();
+    }
+
+    private int[][] rotate() {
+        int[][] newTiles = new int[this.x][this.y];
+            for (int x = 0; x < this.x; x++) {
+                for (int y = 0; y < this.y; y++) {
+                    newTiles[x][y] = tiles[this.y - y - 1][x];
+                }
+            }
+        return newTiles;
+    }
+
+    public void addTile(){
+        List<Integer[]> list = checkSpace();
+        if(!list.isEmpty()){
+            int i = (int) ((Math.random() * list.size()) % list.size());
+            Integer[] coordinates = list.get(i);
+            int newValue = (Math.random() < 0.9 ? 2 : 4);
+            tiles[coordinates[0]][coordinates[1]] = newValue;
+        }
+    }
+
+    public boolean compareLine(Integer[] line1, Integer[] line2){
+        if(line1 == line2){
+            return true;
+        } else if (line1.length != line2.length){
+            return false;
+        }
+        for (int x = 0; x < line1.length; x++){
+            if (line1[x].intValue() != line2[x].intValue()){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public Integer[] moveLine(Integer[] line){
+        LinkedList<Integer> moveList = new LinkedList<Integer>();
+        for (int x = 0; x < this.x; x++){
+            if (line[x] != 0) {
+                moveList.addLast(line[x]);
+            }
+        }
+        if(moveList.size() == 0){
+            return line;
+        } else {
+            Integer[] newLine = new Integer[this.x];
+            ensureSize(moveList, this.x);
+            for (int x = 0; x < this.x; x++){
+                newLine[x] = moveList.removeFirst();
+            }
+            return newLine;
+        }
+    }
+
+    public Integer[] mergeLine(Integer[] line){
+        LinkedList<Integer> mergeList = new LinkedList<Integer>();
+        for(int x = 0; x < this.x && line[x] != 0; x++) {
+            int tileValue = line[x];
+            if(x < this.x -1 && line[x].intValue() == line[x+1].intValue()){
+                tileValue *= 2;
+                isWin();
+                this.score += tileValue;
+                int target = 2048;
+                if (score == target){
+                    this.win = true;
+                }
+                x++;
+            }
+            int newTile = tileValue;
+            mergeList.add(newTile);
+        }
+        if(mergeList.size() == 0){
+            return line;
+        } else {
+            ensureSize(mergeList, this.x);
+            return mergeList.toArray(new Integer[this.x]);
+        }
 
     }
 
-    public void mergeLine(){
-
-    }
-
-    public Tile[] getLine(int y){
-        Tile[] line = new Tile[width];
-        for(int x = 0; x < width; x++){
-            line[x] = tiles[x][y];
+    public Integer[] getLine(int x){
+        Integer[] line = new Integer[this.x];
+        for(int y = 0; y < this.y; y++){
+            line[y] = tiles[x][y];
         }
         return line;
     }
 
+    public void setLine(int x, Integer[] line){
+        for(int y = 0; y < this.y; y++){
+            tiles[x][y] = line[y];
+        }
+    }
+
+    private static void ensureSize(java.util.List<Integer> l, int s) {
+        while (l.size() != s) {
+            l.add(0);
+        }
+    }
+
+    public boolean isWin() {
+        for(int x = 0; x < this.x; x++) {
+            for (int y = 0; y < this.y; y++) {
+                if (tiles[x][y] == 2048) {
+                    return win = true;
+                }
+            }
+        }
+        return win = false;
+    }
+
+    public boolean isLose(){
+        if(!isWin() && !movePossible()){
+            return this.lose = true;
+        }
+        return false;
+    }
+
+    public int getScore(){
+        return this.score;
+    }
+
+    public void printBoard(){
+        for(int[] x: tiles){
+            System.out.format("%5d%5d%5d%5d%n", x[0], x[1], x[2], x[3]);
+        }
+        System.out.format("%n");
+    }
 }
--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -1,8 +1,53 @@
 package game;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
 public class Main {
 
-    public static void main(String [] args) {
-
+    public static void main(String [] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        Board gameBoard = new Board(4,4);
+        gameBoard.addTile();
+        gameBoard.printBoard();
+        System.out.println("up = e , left = s , down = d , right = f , restart game = r");
+        while(!gameBoard.isWin()) {
+            if (gameBoard.isWin()){
+                System.out.println("You won!!!");
+            } else if (gameBoard.isLose()){
+                System.out.println("You lost, try again with pressing r");
+            }
+            line = br.readLine();
+            if (line.charAt(0) == 'r') {
+                gameBoard = new Board(4, 4);
+                gameBoard.isWin();
+                gameBoard.isLose();
+                gameBoard.addTile();
+                gameBoard.printBoard();
+            }
+            if (!gameBoard.isWin() && !gameBoard.isLose()) {
+                switch (line.charAt(0)) {
+                    case 's':
+                        gameBoard.left();
+                        break;
+                    case 'f':
+                        gameBoard.right();
+                        break;
+                    case 'e':
+                        gameBoard.up();
+                        break;
+                    case 'd':
+                        gameBoard.down();
+                        break;
+                }
+                System.out.println("Score: " + gameBoard.getScore());
+                gameBoard.printBoard();
+            }
+        }
     }
+
 }
+
