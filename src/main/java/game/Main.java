35eefafa55422c19089718f056a82920bd24e891
jscharein <jscharein@voipfuture.com>
2019-01-23 09:33:38 +0100

    fixed string out of bounds when just pressing enter

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -13,33 +13,35 @@ public class Main {
         final Board gameBoard = new Board(4,4);
         System.out.println("up = e , left = s , down = d , right = f , restart game = r");
         gameBoard.printBoard();
-        while(!gameBoard.isWin()) {
+        while(!gameBoard.isWin() || !gameBoard.isLose()) {
             line = br.readLine();
-            if (line.charAt(0) == 'r') {
-                gameBoard.resetBoard();
-            }
-            if (!gameBoard.isWin() && !gameBoard.isLose()) {
-                switch (line.charAt(0)) {
-                    case 's':
-                        gameBoard.left();
-                        break;
-                    case 'f':
-                        gameBoard.right();
-                        break;
-                    case 'e':
-                        gameBoard.up();
-                        break;
-                    case 'd':
-                        gameBoard.down();
-                        break;
+            if(!line.isEmpty()) {
+                if (line.charAt(0) == 'r') {
+                    gameBoard.resetBoard();
                 }
-                if (gameBoard.isWin()){
-                    System.out.println("You won!!!");
-                } else if (gameBoard.isLose()){
-                    System.out.println("You lost, try again with pressing r");
+                if (!gameBoard.isWin() && !gameBoard.isLose()) {
+                    switch (line.charAt(0)) {
+                        case 's':
+                            gameBoard.left();
+                            break;
+                        case 'f':
+                            gameBoard.right();
+                            break;
+                        case 'e':
+                            gameBoard.up();
+                            break;
+                        case 'd':
+                            gameBoard.down();
+                            break;
+                    }
+                    if (gameBoard.isWin()) {
+                        System.out.println("You won!!!");
+                    } else if (gameBoard.isLose()) {
+                        System.out.println("You lost, try again with pressing r");
+                    }
+                    System.out.println("Score: " + gameBoard.getScore() + " High Score: " + gameBoard.getHighScore());
+                    gameBoard.printBoard();
                 }
-                System.out.println("Score: " + gameBoard.getScore() + " High Score: " + gameBoard.getHighScore());
-                gameBoard.printBoard();
             }
         }
     }
