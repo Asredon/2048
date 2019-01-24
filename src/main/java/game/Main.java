e30f87c2de3f51201b9e84eff23e223316b64346
jscharein <jscharein@voipfuture.com>
2019-01-24 13:29:33 +0100

    removed code that was used for testing rotation

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -14,11 +14,6 @@ public class Main {
         line = br.readLine();
         final Board gameBoard = new Board(Integer.parseInt(line),Integer.parseInt(line));
         System.out.println("up = e , left = s , down = d , right = f , restart game = r");
-        gameBoard.setTiles(0,0,1);
-        gameBoard.setTiles(0,1,3);
-        gameBoard.setTiles(1,0,5);
-        gameBoard.printBoard();
-        gameBoard.rotate();
         gameBoard.printBoard();
         while(!gameBoard.isWin() || !gameBoard.isLose()) {
             line = br.readLine();
